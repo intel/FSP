4b03aaf9c96e311a5ea144482e22ab9575fee131
ClientSysFWGit <clientsysfwgit@intel.com>
2018-02-20 11:36:12 -0800

    Kaby Lake FSP 3.1.1

--- KabylakeFspBinPkg/Include/FsptUpd.h
+++ KabylakeFspBinPkg/Include/FsptUpd.h
@@ -1,7 +1,7 @@
 /** @file
 
   @copyright
-  Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2018, Intel Corporation. All rights reserved.<BR>
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
