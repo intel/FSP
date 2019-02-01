d8f39f65652ec7b040b2c34a99f520982a5d6ba3
ClientSysFWGit <clientsysfwgit@intel.com>
2019-01-31 18:04:00 -0800

    Coffee Lake FSP 7.0.58.40

--- CoffeeLakeFspBinPkg/Include/SmbiosCacheInfoHob.h
+++ CoffeeLakeFspBinPkg/Include/SmbiosCacheInfoHob.h
@@ -2,15 +2,9 @@
   Header file for SMBIOS Cache Info HOB
 
  @copyright
-  Copyright (c) 2015 - 2018, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2015 - 2019, Intel Corporation. All rights reserved.<BR>
 
-  This program and the accompanying materials are licensed and made available under
-  the terms and conditions of the BSD License which accompanies this distribution.
-  The full text of the license may be found at
-  http://opensource.org/licenses/bsd-license.php
-
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 
   System Management BIOS (SMBIOS) Reference Specification v3.1.0
   dated 2016-Nov-16 (DSP0134)
--- CoffeeLakeFspBinPkg/Include/SmbiosProcessorInfoHob.h
+++ CoffeeLakeFspBinPkg/Include/SmbiosProcessorInfoHob.h
@@ -2,14 +2,9 @@
   Header file for SMBIOS Processor Info HOB
 
  @copyright
-  Copyright (c) 2015 - 2018, Intel Corporation. All rights reserved.<BR>
-  This program and the accompanying materials are licensed and made available under
-  the terms and conditions of the BSD License that accompanies this distribution.
-  The full text of the license may be found at
-  http://opensource.org/licenses/bsd-license.php.
+  Copyright (c) 2015 - 2019, Intel Corporation. All rights reserved.<BR>
 
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 
   System Management BIOS (SMBIOS) Reference Specification v3.1.0
   dated 2016-Nov-16 (DSP0134)
