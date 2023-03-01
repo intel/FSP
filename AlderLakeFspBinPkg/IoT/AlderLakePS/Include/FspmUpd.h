f130444806ad33279bc580df35442fd2c04ecb48
swong23 <swee.heng.wong@intel.com>
2023-03-01 14:25:44 +0800

    IoT ADL-PS MR2 (4022_00) FSP

--- AlderLakeFspBinPkg/IoT/AlderLakePS/Include/FspmUpd.h
+++ AlderLakeFspBinPkg/IoT/AlderLakePS/Include/FspmUpd.h
@@ -1,6 +1,6 @@
 /** @file
 
-Copyright (c) 2022, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
@@ -1165,11 +1165,18 @@ typedef struct {
 **/
   UINT8                       RealtimeMemoryFrequency;
 
-/** Offset 0x02B0 - SaPreMemProductionRsvd
+/** Offset 0x02B0 - OC Safe Mode
+  0: Disabled, 1(Default): Enabled. Ignored unless SpdProfileSelected is an XMP Profile.
+  If enabled, MRC will use less aggressive controls when training memory.
+  $EN_DIS
+**/
+  UINT8                       OCSafeMode;
+
+/** Offset 0x02B1 - SaPreMemProductionRsvd
   Reserved for SA Pre-Mem Production
   $EN_DIS
 **/
-  UINT8                       SaPreMemProductionRsvd[97];
+  UINT8                       SaPreMemProductionRsvd[96];
 
 /** Offset 0x0311 - Enable Gt CLOS
   0(Default)=Disable, 1=Enable
