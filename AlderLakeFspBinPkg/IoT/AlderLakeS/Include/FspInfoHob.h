3beceb01f90dbdbe7781c7fa43ecc928cd68d2f0
swong23 <swee.heng.wong@intel.com>
2023-06-30 14:45:10 +0800

    IoT ADL-S MR5 (4081_05) FSP

--- AlderLakeFspBinPkg/IoT/AlderLakeS/Include/FspInfoHob.h
+++ AlderLakeFspBinPkg/IoT/AlderLakeS/Include/FspInfoHob.h
@@ -2,7 +2,7 @@
   Header file for FSP Information HOB.
 
 @copyright
-  INTEL CONFIDENTIAL
+  
   Copyright 2017 - 2019 Intel Corporation.
 
   The source code contained or described herein and all documents related to the
