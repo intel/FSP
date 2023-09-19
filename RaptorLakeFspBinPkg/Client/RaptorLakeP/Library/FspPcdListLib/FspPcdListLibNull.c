55e25b819e62ccd1962de8880147737081289577
Nate DeSimone <nathaniel.l.desimone@intel.com>
2023-09-18 17:07:45 -0700

    Raptor Lake FSP C.1.BD.40

---
+++ RaptorLakeFspBinPkg/Client/RaptorLakeP/Library/FspPcdListLib/FspPcdListLibNull.c
@@ -0,0 +1,25 @@
+/** @file
+  Library instance to list all DynamicEx PCD FSP consumes.
+  No real functionality.
+
+@copyright
+  Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+@par Specification Reference:
+**/
+
+#include <Base.h>
+
+/**
+  Do nothing function.
+
+**/
+VOID
+FspPcdListLibNull (
+  VOID
+  )
+{
+  return;
+}
