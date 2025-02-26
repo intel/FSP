6e84fc2ed0f6dec196a602eb1638165bb989d3fa
LowLJ1999 <li.jing.low@intel.com>
2025-02-26 10:58:16 +0800

    IoT ADL-N MR6 (6023_00)

--- AlderLakeFspBinPkg/IoT/AlderLakeN/Include/FspInfoHob.h
+++ AlderLakeFspBinPkg/IoT/AlderLakeN/Include/FspInfoHob.h
@@ -5,16 +5,6 @@
 
   Copyright 2017 - 2019 Intel Corporation.
 
-  The source code contained or described herein and all documents related to the
-  source code ("Material") are owned by Intel Corporation or its suppliers or
-  licensors. Title to the Material remains with Intel Corporation or its suppliers
-  and licensors. The Material may contain trade secrets and proprietary and
-  confidential information of Intel Corporation and its suppliers and licensors,
-  and is protected by worldwide copyright and trade secret laws and treaty
-  provisions. No part of the Material may be used, copied, reproduced, modified,
-  published, uploaded, posted, transmitted, distributed, or disclosed in any way
-  without Intel's prior express written permission.
-
   No license under any patent, copyright, trade secret or other intellectual
   property right is granted to or conferred upon you by disclosure or delivery
   of the Materials, either expressly, by implication, inducement, estoppel or
